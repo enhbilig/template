5e4672b43a64c1a030be6c02301d7e737196e549
biligee <lol_surprise@yahoo.com>
2020-10-13 13:27:21 +0800

    tusliin anhnii file uusgeh

---
+++ dadlaga.js
@@ -0,0 +1,8 @@
+var mashin = ['prius10', 'prius11', 'prius20', 'prius40']
+/* mashin[0] = [prius10]
+mashin[1] = [prius11]
+mashin[2] = [prius20]
+mashin[3] = [prius30]*/
+mashin[3] = 'prius30';
+
+document.getElementById("Jishee").innerHTML = (mashin);
