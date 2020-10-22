47241010e15e9a5048f1aae41773f90e5e16fbe6
biligee <lol_surprise@yahoo.com>
2020-10-22 16:12:28 +0800

    lastVersion

--- dadlaga.js
+++
@@ -1,8 +0,0 @@
-var mashin = ['prius10', 'prius11', 'prius20', 'prius40']
-/* mashin[0] = [prius10]
-mashin[1] = [prius11]
-mashin[2] = [prius20]
-mashin[3] = [prius30]*/
-mashin[3] = 'prius30';
-
-document.getElementById("Jishee").innerHTML = (mashin);
